8c6479ab7f1ff7a7ecb0ce07c52f4319d1872a28
trak-dev <yann.picaud@gmail.com>
2021-04-01 10:08:29 +0200

    projet mern back OK front NOK

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are in Edit Exercises list component ! </p>
+            </div>
+        )
+    }
+}
